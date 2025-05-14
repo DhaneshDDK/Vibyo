541f708f4a8f93d2883dfff82c46108907e57193
DhaneshDDK <dandedhanesh899@gmail.com>
2025-05-14 21:50:59 +0530

    code change #2

--- server/Routes/Auth.js
+++ server/Routes/Auth.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 const {Login, Register,Remove, Logout} = require('../Controllers/Registration.js');
+const {verifyToken} = require('../Middlewares/Auth.js')
 
 router.post("/login",Login);
 router.post("/register",Register);
 router.delete("/remove",Remove);
 router.get("/logout",Logout);
+
+router.post('/protectedRoute',verifyToken,(req,res)=>{
+    res.status(200).json({
+        message : "You can access this route",
+        data : req.user
+    })
+})
 
 module.exports = router;
